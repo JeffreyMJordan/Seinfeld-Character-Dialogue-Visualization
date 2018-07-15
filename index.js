ad030bd19c63a7273c447c5fe788780cc1f4599b
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2018-07-14 19:02:40 -0700

    Completes xScale and yScale

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+
+let characters = ['ELAINE', 'JERRY', 'GEORGE', 'KRAMER', 'NEWMAN'];
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("here");
+  fetch('./data.json').then((res) => {
+    return res.json();
+  }).then((data) => {
+    d3.select("#graph");
+    let xScale = d3.scaleBand()
+      .rangeRound([0, window.innerWidth - 20])
+      .paddingInner(0.05)
+      .domain(data.map((episode) => {return episode["TITLE"]}));
+
+    let yScale = d3.scaleLinear()
+      .rangeRound([window.innerHeight, 0])
+      .domain([0, d3.max(data, function(d){
+        let total = 0;
+        Object.keys(d).forEach(key => {
+          let num = d[key];
+          if(typeof(num) !== 'string'){
+            total += num;
+          }
+        });
+        return total;
+      })]);
+    debugger;
+  });
 });
